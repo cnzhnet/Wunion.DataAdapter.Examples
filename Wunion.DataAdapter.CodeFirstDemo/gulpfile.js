631e0b04c0692c99eaa9898937547d69f78041a5
巽翎君 <cnzhnet@hotmail.com>
2022-02-27 01:32:11 +0800

    更改忽略文件

---
+++ Wunion.DataAdapter.CodeFirstDemo/gulpfile.js
@@ -0,0 +1,33 @@
+﻿/// <binding AfterBuild='default' Clean='clean' />
+
+const gulp = require('gulp');
+const del = require('del');
+const uglify = require('gulp-uglify');
+
+var paths = {
+    scripts: [
+        'Client/**/*.js',
+        'Client/**/*.ts',
+        'Client/**/*.map'
+        //'!Client/**/*.d.ts'
+    ],
+    lib: []
+};
+
+// 执行清理任务.
+gulp.task('clean', function () {
+    return del(['wwwroot/js/**/*', 'Client/**/*.js', 'Client/**/*.map']);
+});
+
+// 仅清除编译的 js
+gulp.task('clean:src', function () {
+    return del(['Scripts/**/*.js', 'Scripts/**/*.map']);
+});
+
+gulp.task('default', function (done) {
+    gulp.src(paths.scripts)
+        .pipe(gulp.dest('wwwroot/js'));
+        //.pipe(uglify())
+        //.pipe(gulp.dest('wwwroot/js'));
+    done();
+});
